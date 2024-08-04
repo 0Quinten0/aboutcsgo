c997d161d40a97e1453ad203ed3a861b3fe64402
Quinten <quintenbijl@hotmail.com>
2024-08-04 22:49:33 +0200

    Cors open

--- config/cors.php
+++ config/cors.php
@@ -19,13 +19,13 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://aboutcsgo.com'], // specific domain
+    'allowed_origins' => ['*'],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ['*'],
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
+    'exposed_headers' => ['*'],
 
     'max_age' => 0,
 
