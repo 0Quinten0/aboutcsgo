516b95c92af8a97fa0b59e1c28d1e6b9ce4088d1
Quinten <quintenbijl@hotmail.com>
2025-04-09 11:55:03 +0200

    https axiosclient

--- resources/js/axiosClient.tsx
+++ resources/js/axiosClient.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an instance of axios with default properties
 const axiosClient = axios.create({
-    baseURL: "http://aboutcsgo.com/api/v1/",
+    baseURL: "https://aboutcsgo.com/api/v1/",
 
     // baseURL: "http://127.0.0.1:8000/api/v1/",
 });
