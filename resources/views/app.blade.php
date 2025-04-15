a9f49d6d6ad3fb71b504ca10ca1027b91dec76ab
Quinten <quintenbijl@hotmail.com>
2025-04-15 15:49:14 +0200

    html lang fixed

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <meta http-equiv="content-language" content="en">
     @vite('resources/js/app.jsx')
     @inertiaHead
   </head>
